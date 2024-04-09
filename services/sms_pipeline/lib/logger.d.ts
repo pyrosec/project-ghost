220c9aceb3817e769250b94c6e8e9b16038d7ff7
ghostdial <ghostdialfoundation@gmail.com>
2024-04-09 23:28:00 +0000

    fixes

--- services/sms_pipeline/lib/logger.d.ts
+++ services/sms_pipeline/lib/logger.d.ts
@@ -1,1 +1,6 @@
-export declare const logger: import("@ghostdial/logger").GhostLogger;
+import { createLogger as createWinstonLogger } from "winston";
+export interface GhostLogger extends ReturnType<typeof createWinstonLogger> {
+    star(v: any): GhostLogger;
+}
+export declare const createLogger: (proc?: string) => GhostLogger;
+export declare const logger: GhostLogger;
