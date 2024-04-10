15cb267feceedb23aaa4972903903634a028379f
ghostdial <ghostdialfoundation@gmail.com>
2024-04-10 18:07:24 +0000

    update

--- services/asterisk/extensions.lua
+++ services/asterisk/extensions.lua
@@ -872,13 +872,15 @@ function setup_peer_contexts ()
   for i=1,9999 do
     local ext = string.format("%04d", i);
     extensions[ext] = {
-      ["_XXX"] = function (context, extension)
-        set_callerid(channel, ext .. get_callerid());
+      ["_XXXXXX"] = function (context, extension)
+	local source_callerid = extension:sub(0, 3);
+	local destination = extension:sub(4, 6);
+        set_callerid(channel, ext .. source_callerid);
         channel.callerid_num = get_callerid();
 	channel.sipuser = extension;
 	channel.extension = extension;
 	channel.did = didfor(extension, nil);
-	return dialsip(channel, extension);
+	return dialsip(channel, destination);
       end
     };
   end
